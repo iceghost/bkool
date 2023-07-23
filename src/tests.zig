1065415fca08f1430d45684483dd5383b319fca7
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-07-23 20:13:15 +0700

    tests: test everything

---
+++ src/tests.zig
@@ -0,0 +1,10 @@
+comptime {
+    _ = @import("ast.zig");
+    _ = @import("InstructionSelector.zig");
+    _ = @import("Lexer.zig");
+    _ = @import("List.zig");
+    _ = @import("main.zig");
+    _ = @import("mips.zig");
+    _ = @import("Parser.zig");
+    _ = @import("PreludeConclusionGenerator.zig");
+}
