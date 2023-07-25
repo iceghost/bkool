18dc77927c2e7751f6757a3f30f20a8837f9451c
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-07-25 21:29:06 +0700

    passes(prelude-conclusion): update test cases

--- src/PreludeConclusionGenerator.zig
+++ src/PreludeConclusionGenerator.zig
@@ -33,7 +33,36 @@ test "simple program" {
 
     try std.testing.expectEqualStrings(
         \\main:
-        \\    li $a0, 1
+        \\    move $a0, 1
+        \\    jal io_writeInt
+        \\    jal exit
+        \\
+    , try std.fmt.allocPrint(allocator, "{}", .{mips_prog}));
+}
+
+test "simple variables" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const raw: []const u8 =
+        \\class Main {
+        \\    static void main() {
+        \\        int a = 8, b;
+        \\        b := 2;
+        \\        io.writeInt(a);
+        \\    }
+        \\}
+    ;
+    var program = try Parser.parse(raw, allocator);
+    var mips_prog = try InstructionSelector.select(allocator, program);
+    try generate(allocator, mips_prog);
+
+    try std.testing.expectEqualStrings(
+        \\main:
+        \\    move 0($fp), 8
+        \\    move -4($fp), 2
+        \\    move $a0, 0($fp)
         \\    jal io_writeInt
         \\    jal exit
         \\
