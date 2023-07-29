b6d92df754b18432f545b78842dd1fbcb682668c
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-07-29 20:59:22 +0700

    tests: use snapshot test
    see `src/snaptest/snaptest.zig` for details

--- src/HomeAssigner.zig
+++ src/HomeAssigner.zig
@@ -49,33 +49,3 @@ fn assignInstr(self: *Self, instr: *mips.Instr) Error!void {
         else => {},
     }
 }
-
-const Parser = @import("Parser.zig");
-const InstructionSelector = @import("InstructionSelector.zig");
-
-test "simple variables" {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    var allocator = arena.allocator();
-
-    const raw: []const u8 =
-        \\class Main {
-        \\    static void main() {
-        \\        int a = 8, b;
-        \\        b := 2;
-        \\        io.writeInt(a);
-        \\    }
-        \\}
-    ;
-    var program = try Parser.parse(raw, allocator);
-    var mips_prog = try InstructionSelector.select(allocator, program);
-    try assign(allocator, mips_prog);
-
-    try std.testing.expectEqualStrings(
-        \\    move 0($fp), 8
-        \\    move 4($fp), 2
-        \\    move $a0, 0($fp)
-        \\    jal io_writeInt
-        \\
-    , try std.fmt.allocPrint(allocator, "{}", .{mips_prog}));
-}
--- src/tests.zig
+++ src/tests.zig
@@ -1,10 +1,236 @@
+const Parser = @import("Parser.zig");
+const Snap = @import("snaptest/snaptest.zig").Snap;
+const snap = Snap.snap;
+const std = @import("std");
+const InstructionSelector = @import("InstructionSelector.zig");
+const HomeAssigner = @import("HomeAssigner.zig");
+const InstructionPatcher = @import("InstructionPatcher.zig");
+const PreludeConclusionGenerator = @import("PreludeConclusionGenerator.zig");
+
 comptime {
     _ = @import("ast.zig");
-    _ = @import("InstructionSelector.zig");
     _ = @import("Lexer.zig");
     _ = @import("List.zig");
     _ = @import("main.zig");
     _ = @import("mips.zig");
-    _ = @import("Parser.zig");
-    _ = @import("PreludeConclusionGenerator.zig");
+}
+
+fn skipRemaining() !void {
+    return error.SkipZigTest;
+}
+
+test "simple program" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const raw: []const u8 =
+        \\class Main {
+        \\    static void main() {
+        \\        io.writeInt(1);
+        \\    }
+        \\}
+    ;
+
+    var program = try Parser.parse(raw, allocator);
+    try snap(@src(),
+        \\class Main
+        \\    method main
+        \\        io.writeInt 1
+        \\
+    ).diffFmt("{}", .{program});
+
+    var mips_prog = try InstructionSelector.select(allocator, program);
+    try snap(@src(),
+        \\    move $a0, 1
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try HomeAssigner.assign(allocator, mips_prog);
+    try snap(@src(),
+        \\    move $a0, 1
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try InstructionPatcher.patch(allocator, mips_prog);
+    try snap(@src(),
+        \\    li $a0, 1
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try PreludeConclusionGenerator.generate(allocator, mips_prog);
+    try snap(@src(),
+        \\main:
+        \\    li $a0, 1
+        \\    jal io_writeInt
+        \\    jal exit
+        \\
+    ).diffFmt("{}", .{mips_prog});
+}
+
+test "simple variables" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const raw: []const u8 =
+        \\class Main {
+        \\    static void main() {
+        \\        int a = 8, b;
+        \\        b := 2;
+        \\        io.writeInt(a);
+        \\    }
+        \\}
+    ;
+
+    var program = try Parser.parse(raw, allocator);
+    try snap(@src(),
+        \\class Main
+        \\    method main
+        \\        a: int = 8
+        \\        b: int
+        \\        b := 2
+        \\        io.writeInt a
+        \\
+    ).diffFmt("{}", .{program});
+
+    var mips_prog = try InstructionSelector.select(allocator, program);
+    try snap(@src(),
+        \\    move a, 8
+        \\    move b, 2
+        \\    move $a0, a
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try HomeAssigner.assign(allocator, mips_prog);
+    try snap(@src(),
+        \\    move 0($fp), 8
+        \\    move 4($fp), 2
+        \\    move $a0, 0($fp)
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try InstructionPatcher.patch(allocator, mips_prog);
+    try snap(@src(),
+        \\    li $t0, 8
+        \\    sw $t0, 0($fp)
+        \\    li $t0, 2
+        \\    sw $t0, 4($fp)
+        \\    lw $a0, 0($fp)
+        \\    jal io_writeInt
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try PreludeConclusionGenerator.generate(allocator, mips_prog);
+    try snap(@src(),
+        \\main:
+        \\    li $t0, 8
+        \\    sw $t0, 0($fp)
+        \\    li $t0, 2
+        \\    sw $t0, 4($fp)
+        \\    lw $a0, 0($fp)
+        \\    jal io_writeInt
+        \\    jal exit
+        \\
+    ).diffFmt("{}", .{mips_prog});
+}
+
+test "variables with addition" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const raw: []const u8 =
+        \\class Main {
+        \\    static void main() {
+        \\        int a = 8, b;
+        \\        b := 2;
+        \\        io.writeInt(a + 1);
+        \\        io.writeInt(b + a);
+        \\    }
+        \\}
+    ;
+
+    var program = try Parser.parse(raw, allocator);
+    try snap(@src(),
+        \\class Main
+        \\    method main
+        \\        a: int = 8
+        \\        b: int
+        \\        b := 2
+        \\        io.writeInt (+ a 1)
+        \\        io.writeInt (+ b a)
+        \\
+    ).diffFmt("{}", .{program});
+
+    try skipRemaining();
+
+    var mips_prog = try InstructionSelector.select(allocator, program);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try HomeAssigner.assign(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try InstructionPatcher.patch(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try PreludeConclusionGenerator.generate(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+}
+
+test "associative addition" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const raw: []const u8 =
+        \\class Main {
+        \\    static void main() {
+        \\        io.writeInt(1 + 2 + 3);
+        \\    }
+        \\}
+    ;
+
+    var program = try Parser.parse(raw, allocator);
+    try snap(@src(),
+        \\class Main
+        \\    method main
+        \\        io.writeInt (+ (+ 1 2) 3)
+        \\
+    ).diffFmt("{}", .{program});
+
+    try skipRemaining();
+
+    var mips_prog = try InstructionSelector.select(allocator, program);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try HomeAssigner.assign(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try InstructionPatcher.patch(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
+
+    try PreludeConclusionGenerator.generate(allocator, mips_prog);
+    try snap(@src(),
+        \\
+    ).diffFmt("{}", .{mips_prog});
 }
